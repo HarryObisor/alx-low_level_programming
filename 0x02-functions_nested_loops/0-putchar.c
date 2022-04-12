14b2d9467337a483c60cec3b63cdba4797ae056b
Harry <owaliharry090@gmail.com>
2022-04-12 05:09:09 -0700

    task 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-	char name[] = "_putchar";
-
-	putchar(name[]);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
